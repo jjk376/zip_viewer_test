21707173a62074c8af7cd0e54cc91952c77e2d99
sjbaek0312 <sjbaek0312@naver.com>
2017-07-18 16:20:32 +0900

    Add files via upload

--- src/main/java/com/sj/model/FileVO.java
+++ src/main/java/com/sj/model/FileVO.java
@@ -1,22 +1,18 @@
 package com.sj.model;
 
-import java.util.Date;
-
 public class FileVO {
 
-	private String file_id;
+	private long file_id;
 	private String user_id;
-	private String internal_file_name;
 	private String file_name;
 	private long file_size;
 	private String file_type;
-	private String file_register_date;
-	private String file_modified_date;
+	private String file_register_time;
 	
-	public String getFile_id() {
+	public long getFile_id() {
 		return file_id;
 	}
-	public void setFile_id(String file_id) {
+	public void setFile_id(long file_id) {
 		this.file_id = file_id;
 	}
 	public String getUser_id() {
@@ -24,12 +20,6 @@ public class FileVO {
 	}
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
-	}
-	public String getInternal_file_name() {
-		return internal_file_name;
-	}
-	public void setInternal_file_name(String internal_file_name) {
-		this.internal_file_name = internal_file_name;
 	}
 	public String getFile_name() {
 		return file_name;
@@ -49,18 +39,10 @@ public class FileVO {
 	public void setFile_type(String file_type) {
 		this.file_type = file_type;
 	}
-	public String getFile_register_date() {
-		return file_register_date;
+	public String getFile_register_time() {
+		return file_register_time;
 	}
-	public void setFile_register_date(String file_register_date) {
-		this.file_register_date = file_register_date;
+	public void setFile_register_time(String file_register_time) {
+		this.file_register_time = file_register_time;
 	}
-	public String getFile_modified_date() {
-		return file_modified_date;
-	}
-	public void setFile_modified_date(String file_modified_ate) {
-		this.file_modified_date = file_modified_ate;
-	}
-	
-	
 }
